f45398a472dabd690f9c406d8dd4f6f9ca559a9b
padamczyk <piotr.adamczyk@iteratec.com>
2024-04-26 22:37:02 +0200

    terraform small refactor

--- modules/api/secretManager.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_secretsmanager_secret" "openAIKey" {
-  name        = "openAIKey"
-  description = "Open AI key"
-}
-
-resource "aws_secretsmanager_secret_version" "example" {
-  secret_id     = aws_secretsmanager_secret.openAIKey.id
-  secret_string = jsonencode({key = var.openAIKey})
-}
