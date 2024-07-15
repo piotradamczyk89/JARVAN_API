70efa026e04c17a191aa903a8fcd569ec9e9c53b
padamczyk <piotr.adamczyk@iteratec.com>
2024-07-15 15:47:23 +0200

    lambda raises errors

--- modules/lambda_utils/langchain_layer/layer/python/open_ai_utils/open_ai_utlis.py
+++ modules/lambda_utils/langchain_layer/layer/python/open_ai_utils/open_ai_utlis.py
@@ -1,19 +1,12 @@
 import logging
-from openai import OpenAIError
 from langchain_openai import OpenAIEmbeddings
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
 def get_embeddings(text, ai_key):
-    try:
-        embeddings = OpenAIEmbeddings(model="text-embedding-3-small", openai_api_key=ai_key)
-        value = embeddings.embed_query(text)
-        print(value)
-        return value
-    except OpenAIError as e:
-        logger.error(f"Open AI Error: {e}")
-        raise e
-    except Exception as e:
-        logger.error(f"An unexpected error occurred: {e}")
-        raise e
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small", openai_api_key=ai_key)
+    value = embeddings.embed_query(text)
+    print(value)
+    return value
--- modules/stepFunction/src/memory.py
+++ modules/stepFunction/src/memory.py
@@ -2,9 +2,11 @@ import json
 import logging
 import uuid
 import boto3
+from botocore.exceptions import BotoCoreError
+from openai import OpenAIError
 
 from custom_methods import slack_bot_response
-from models import SecretManagerCache
+from models import SecretManagerCache, MissingSecretException
 from pinecone_utils import get_vector_base_index
 from open_ai_utils import get_embeddings
 
@@ -13,26 +15,38 @@ logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
-    secret_manager_cache = SecretManagerCache()
-    ai_key = secret_manager_cache.get_secret("AIKey")
-    pine_cone_key = secret_manager_cache.get_secret("PineConeApiKey")
-    logger.info(event)
-    arguments = json.loads(event['arguments'])
+    try:
+        try:
+            secret_manager_cache = SecretManagerCache()
+            ai_key = secret_manager_cache.get_secret("AIKey")
+            pine_cone_key = secret_manager_cache.get_secret("PineConeApiKey")
+        except (BotoCoreError, MissingSecretException) as e:
+            logger.error(f"Error initializing Secrets Manager client or fetching secrets: {e}")
+            raise e
+        logger.info(event)
+        arguments = json.loads(event['arguments'])
+        try:
+            vector_emb = get_embeddings(arguments['memory'], ai_key)
+        except OpenAIError as e:
+            logger.error(f"Open AI Error: {e}")
+            raise e
+        id_ = str(uuid.uuid4())
+        metadata = event["metadata"].copy()
+        metadata.update({"memory": arguments['memory']})
+        vector = {
+            "id": id_,
+            "values": vector_emb,
+            "metadata": metadata
+        }
 
-    vector_emb = get_embeddings(arguments['memory'], ai_key)
-    id_ = str(uuid.uuid4())
-    metadata = event["metadata"].copy()
-    metadata.update({"memory": arguments['memory']})
-    vector = {
-        "id": id_,
-        "values": vector_emb,
-        "metadata": metadata
-    }
-    index = get_vector_base_index(pine_cone_key)
-    index.upsert([vector])
-    del vector["values"]
-    create_memory(vector)
-    slack_bot_response("zapamiętałem")
+        index = get_vector_base_index(pine_cone_key)
+        index.upsert([vector])
+        del vector["values"]
+        create_memory(vector)
+        slack_bot_response("zapamiętałem")
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        raise e
 
 
 def create_memory(memory):
